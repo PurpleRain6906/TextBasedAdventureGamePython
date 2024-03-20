7c53b22ca6b3e9067348e1dcfef778c76a202af5
PurpleRain <124352814+PurpleRain6906@users.noreply.github.com>
2024-03-20 15:15:56 +0000

    Update TextBasedAdventureGame.py

--- TextBasedAdventureGame.py
+++ TextBasedAdventureGame.py
@@ -14,8 +14,8 @@ forestLocationX = [-13,-12,-11,-10,-9,-8,-7,-6,-5,-4]
 forestLocationY = [-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11]
 desertLocationX = [-3,-2,-1,0,1,2,3,4,5,6,7,8]
 desertLocationY = [4,5,6,7,8,9,10,11,12,13]
-BorderX = [9,-14]
-BorderY = [-6,14]
+BorderLocationX = [9,-14]
+BorderLocationY = [-6,14]
 
 
 
